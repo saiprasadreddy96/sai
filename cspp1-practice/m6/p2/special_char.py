bea20ca8513d99e157ab80dd2343217d5639c99a
saiprasadreddy96 <mspreddy1996@msitprogram.net>
2018-08-04 10:21:03 +0530

    " -> 7 of 7 passed. pylint: 7.78/10 "

--- cspp1-practice/m6/p2/special_char.py
+++ cspp1-practice/m6/p2/special_char.py
@@ -1,10 +1,7 @@
-'''
-Replace all the special characters(!, @, #, $, %, ^, &, *) in a given string with a space.
-example : ab!@#cd is the input, the output is ab   cd
-Output has three spaces, which are to be replaced with these special characters
-'''
-def main():
-    S = input()
+"""
+special_char
+"""
+S = input()
 S1 = ""
 I = 0
 while I < len(S):
@@ -14,7 +11,3 @@ while I < len(S):
         S1 = S1 + S[I]
     I = I + 1
 print(S1)
-
-
-if __name__ == "__main__":
-    main()

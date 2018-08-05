6202269cdbd8a7f55295b93f2730f168df34d607
saiprasadreddy96 <mspreddy1996@msitprogram.net>
2018-08-05 22:35:46 +0530

    " -> 3 of 3 passed. pylint: 9.09/10 "

---
+++ cspp1-practice/m5/p3/square_root_bisection.py
@@ -0,0 +1,15 @@
+"""
+SQUARE ROOT BISECTOIN
+"""
+A = int(input())
+EPSILON = 0.01
+LOW = 0.0
+HIGH = A
+ANS = (HIGH + LOW)/2.0
+while abs(ANS**2 - A) >=EPSILON:
+    if ANS**2 < A:
+        LOW = ANS
+    else:
+        HIGH = ANS
+    ANS = (HIGH + LOW)/2.0
+print(ANS)

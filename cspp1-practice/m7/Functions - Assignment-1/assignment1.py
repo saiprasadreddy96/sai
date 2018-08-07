bba6ff64c2117094fc645e3c2ac8a68a7d3ac7a6
saiprasadreddy96 <mspreddy1996@msitprogram.net>
2018-08-07 19:51:10 +0530

    " -> 10 of 10 passed. pylint: -1.11/10 "

---
+++ cspp1-practice/m7/Functions - Assignment-1/assignment1.py
@@ -0,0 +1,16 @@
+
+
+
+def payingDebtOffInAYear(balance, annualInterestRate, monthlyPaymentRate):
+        
+	
+
+def main():
+	data = input()
+	data = data.split(' ')
+	data = list(map(float, data))
+	print(payingDebtOffInAYear(data[0],data[1],data[2]))
+
+if __name__== "__main__":
+	main()
+

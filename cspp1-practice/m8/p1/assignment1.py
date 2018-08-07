ae6c89dc944fbebdfbe11455e7e3bd7c787b2437
saiprasadreddy96 <mspreddy1996@msitprogram.net>
2018-08-07 20:38:08 +0530

    " -> 10 of 10 passed. pylint: 8.89/10 "

--- cspp1-practice/m8/p1/assignment1.py
+++ cspp1-practice/m8/p1/assignment1.py
@@ -2,18 +2,21 @@
 """
 fact
 """
-def factorial(var_N):
+def factorial(var_n):
     '''
     n is positive Integer
 
     returns: a positive integer, the factorial of n
     '''
-    if var_N in [0, 1]:
+    if var_n in [0, 1]:
         return 1
-    return var_N*factorial(var_N-1)
+    return var_n * factorial(var_n-1)
 def main():
-    var_A = int(input())
-    print(factorial(int(var_A)))    
+    """
+    main
+    """
+    var_a = int(input())
+    print(factorial(int(var_a)))    
 
 if __name__ == "__main__":
     main()

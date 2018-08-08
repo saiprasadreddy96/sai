f7d88fca8d60b33625bbb03f2b1a68ace7930ba6
saiprasadreddy96 <mspreddy1996@msitprogram.net>
2018-08-08 18:56:05 +0530

    functions and objects

---
+++ cspp1-practice/m9/Functions and Objects Exercise-1/functions_and_objects_1.py
@@ -0,0 +1,19 @@
+#Exercise : Function and Objects Exercise-1
+#Implement a function that converts the given testList = [1, -4, 8, -9] into [1, 4, 8, 9]
+
+
+def apply_to_each(L, f):
+    for k in range(len(L)):
+        L[k] = f(L[k])
+    print(L)
+    
+def main():
+    data = input()
+    data = data.split(" ")
+    list1 = []
+    for j in data:
+        list1.append(int(j))
+    apply_to_each(list1, abs)
+
+if __name__ == "__main__":
+    main()

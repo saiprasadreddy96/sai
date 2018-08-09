5e45308c45cf73d2bc7f4bd5141a37114ebbe1b6
saiprasadreddy96 <mspreddy1996@msitprogram.net>
2018-08-09 21:05:10 +0530

    " -> 10 of 10 passed. pylint: 5.88/10 "

---
+++ cspp1-practice/m10/p1/assignment1.py
@@ -0,0 +1,39 @@
+'''
+Exercise : Assignment-1
+implement the function get_available_letters that takes in one parameter -
+a list of letters, letters_guessed. This function returns a string
+that is comprised of lowercase English letters - all lowercase English letters
+that are not in letters_guessed
+'''
+
+def get_available_letters(letters_guessed):
+    '''
+    :param letters_guessed: list, what letters have been guessed so far
+    returns: string, comprised of letters that represents what letters have not
+      yet been guessed.
+    '''
+    s="abcdefghijklmnopqrstuvwxyz"
+    l=[]
+    l=list(s)
+    for char in letters_guessed:
+        if char in l:
+            l.remove(char)
+    
+    return ''.join(l)
+
+
+
+def main():
+    '''
+    Main function for the given program
+    '''
+    user_input = input()
+    user_input = user_input.split(" ")
+    data = []
+    for char in user_input:
+        data.append(char)
+    print(get_available_letters(data))
+
+
+if __name__ == "__main__":
+    main()

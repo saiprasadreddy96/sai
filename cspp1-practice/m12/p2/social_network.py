952908b6298ed53dfcb7b214eb3c14a7a628afc7
saiprasadreddy96 <mspreddy1996@msitprogram.net>
2018-08-11 11:59:05 +0530

    " -> 1 of 2 passed. pylint: 9.00/10 "

--- cspp1-practice/m12/p2/social_network.py
+++ cspp1-practice/m12/p2/social_network.py
@@ -14,8 +14,7 @@ def follow(network, arg1, arg2):
         update the network dictionary and return it
     '''
     # remove the pass below and start writing your code
-    if arg2 not in network[arg1]:
-        network[arg1].append(arg2)
+    network[arg1].append(arg2)
     return network
 
 def unfollow(network, arg1, arg2):

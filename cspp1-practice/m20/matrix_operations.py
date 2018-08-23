b0c43eb5ac8ca9c96707453af9e5f723451ca43e
saiprasadreddy96 <mspreddy1996@msitprogram.net>
2018-08-23 19:44:25 +0530

    " -> 7 of 8 passed. pylint: 6.88/10 "

--- cspp1-practice/m20/matrix_operations.py
+++ cspp1-practice/m20/matrix_operations.py
@@ -1,4 +1,4 @@
-def mult_matrix(m1, m2):
+def mult_matrix(matrix_1, matrix_2):
     '''
         check if the matrix1 columns = matrix2 rows
         mult the matrices and return the result matrix
@@ -6,22 +6,22 @@ def mult_matrix(m1, m2):
         and return None
         error message should be "Error: Matrix shapes invalid for mult"
     '''
-    m4 = []
-    l1 = []
-    if r2 == c1:
-        for i in range(r1):
-            for j in range(c2):
-                sum = 0
-                for k in range(r2):
-                    sum = sum + ((m1[i][k]) * (m2[k][j]))
-                l1.append(sum)
-            m4.append(l1)
-            l1 = []
-        return m4
+    matrix_4 = []
+    list_1 = []
+    if rows_m2 == columns_m1:
+        for i in range(rows_m1):
+            for j in range(columns_m2):
+                temp_sum = 0
+                for k in range(rows_m2):
+                    temp_sum = temp_sum + ((matrix_1[i][k]) * (matrix_2[k][j]))
+                list_1.append(temp_sum)
+            matrix_4.append(list_1)
+            list_1 = []
+        return matrix_4
     else:
         print("Error: Matrix shapes invalid for mult")
         return None
-def add_matrix(m1, m2):
+def add_matrix(matrix_1, matrix_2):
     '''
         check if the matrix shapes are similar
         add the matrices and return the result matrix
@@ -29,15 +29,15 @@ def add_matrix(m1, m2):
         and return None
         error message should be "Error: Matrix shapes invalid for addition"
     '''
-    m3 = []
-    l1 = []
+    matrix_3 = []
+    list_1 = []
     try:
-        for i in range(r1):
-            for j in range(c1):
-                l1.append((m1[i][j]) + (m2[i][j]))
-            m3.append(l1)
-            l1 = []
-        return m3
+        for i in range(rows_m1):
+            for j in range(columns_m1):
+                list_1.append((matrix_1[i][j]) + (matrix_2[i][j]))
+            matrix_3.append(list_1)
+            list_1 = []
+        return matrix_3
     except:
         print("Error: Matrix shapes invalid for addition")
         return None
@@ -53,12 +53,12 @@ def read_matrix(r, c):
     try:
         for i in range(r):
             temp = input()
-            l1 = temp.split()
-            l1 = list(map(int,l1))
-            m.append(l1)
-            if len(l1) != c:
-               print("Error: Invalid input for the matrix")
-               return None 
+            list_1 = temp.split()
+            list_1 = list(map(int, list_1))
+            m.append(list_1)
+            if len(list_1) != c:
+                print("Error: Invalid input for the matrix")
+                return None
         return m
     except:
         print("Error: Invalid input for the matrix")
@@ -73,21 +73,21 @@ def main():
     # multiply matrix 1 and matrix 2
     """main"""
     temp = input()
-    global r1, c1, r2, c2
-    r1 = int(temp[0:1])
-    c1 = int(temp[2:])
-    m1 = []
-    m1 = read_matrix(r1, c1)
+    global rows_m1, columns_m1, rows_m2, columns_m2
+    rows_m1 = int(temp[0:1])
+    columns_m1 = int(temp[2:])
+    matrix_1 = []
+    matrix_1 = read_matrix(rows_m1, columns_m1)
     temp = input()
-    r2 = int(temp[0:1])
-    c2 = int(temp[2:])
-    m2 = []
-    m2 = read_matrix(r2, c2)
-    m3 = []; m4 = []
-    if m1 != None and m2 != None:
-        m3 = add_matrix(m1, m2)
-        print(m3)
-        m4 = mult_matrix(m1, m2)
-        print(m4)
+    rows_m2 = int(temp[0:1])
+    columns_m2 = int(temp[2:])
+    matrix_2 = []
+    matrix_2 = read_matrix(rows_m2, columns_m2)
+    matrix_3 = []; matrix_4 = []
+    if matrix_1 != None and matrix_2 != None:
+        matrix_3 = add_matrix(matrix_1, matrix_2)
+        print(matrix_3)
+        matrix_4 = mult_matrix(matrix_1, matrix_2)
+        print(matrix_4)
 if __name__ == '__main__':
     main()

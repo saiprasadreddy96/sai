8c3f27b2c48c829cb1561a12f9a49db99f06b0fc
saiprasadreddy96 <mspreddy1996@msitprogram.net>
2018-08-24 14:38:14 +0530

    " -> 14 of 14 passed. pylint: 4.90/10 "

--- cspp1-practice/m21/Tic Tac Toe.py
+++ cspp1-practice/m21/Tic Tac Toe.py
@@ -1,6 +1,7 @@
 global TTT, pos_input, count_input, x, o
 TTT = [];pos_input = ['o', 'x', '.'];count_input = {'o':0, 'x':0, '.':0};x = ['x', 'x', 'x'];o = ['o', 'o', 'o']
 def game_TTT():
+    """game ttt"""
     global c_x, c_o
     c_x = 0;c_o = 0
     for i in range(3):
@@ -13,6 +14,7 @@ def game_TTT():
     if [TTT[0][0], TTT[1][1], TTT[2][2]] == o or [TTT[0][2], TTT[1][1], TTT[2][0]] == o:
         c_o += 1
 def check_invalid():
+    """invalid game"""
     for i in range(3):
         for j in range(3):
             if TTT[i][j] not in pos_input:
@@ -22,6 +24,7 @@ def check_invalid():
     #print(count_input['o'])
     return 1
 def read_input():
+    """read input"""
     str_line = []
     for i in range(3):
         str_line = input()
@@ -29,6 +32,7 @@ def read_input():
         TTT.append(str_line)
     #print(TTT)
 def write_output():
+    """write output"""
     if (check_invalid() == 0):
         print("invalid input")
     elif (count_input['o'] != count_input['x'] + 1) and (count_input['x'] != count_input['o'] + 1):
@@ -44,6 +48,7 @@ def write_output():
         else:
             print("draw")
 def main():
+    """main"""
     read_input()
     write_output()
 main()

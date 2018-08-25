967746e4471cbd538c2f0c30b633f8c278862f5c
saiprasadreddy96 <mspreddy1996@msitprogram.net>
2018-08-25 12:58:36 +0530

    " -> 0 of 3 passed. pylint: 7.50/10 "

--- cspp1-practice/m22/assignment4/print_dictionary.py
+++ cspp1-practice/m22/assignment4/print_dictionary.py
@@ -5,7 +5,23 @@ the key and frequency with a SPACE - SPACE.
 '''
 
 def print_dictionary(dictionary):
-    pass
+    temp_l = []
+    temp_l.extend(dictionary.keys())
+    #print(temp_l)
+    for i in range(len(temp_l)-1):
+        j = i + 1
+        while j < len(temp_l):
+            char_1 = temp_l[i]
+            char_2 = temp_l[j]
+            if char_1[0] > char_2[0]:
+                temp = temp_l[i]
+                temp_l[i] = temp_l[j]
+                temp_l[j] = temp
+            j += 1
+    #print(temp_l)
+    for each_i in temp_l:
+        print(each_i,' - ', dictionary[each_i])
+        
 
 def main():
     dictionary = eval(input())

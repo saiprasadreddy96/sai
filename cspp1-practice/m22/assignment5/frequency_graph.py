7e12f7fa52cb2f8aa09c56356a843440ec35d312
saiprasadreddy96 <mspreddy1996@msitprogram.net>
2018-08-25 13:06:26 +0530

    " -> 0 of 4 passed. pylint: 0/0 "

--- cspp1-practice/m22/assignment5/frequency_graph.py
+++ cspp1-practice/m22/assignment5/frequency_graph.py
@@ -5,18 +5,18 @@ frequency of each word. Display the frequency values using “#” as a text bas
 
 def frequency_graph(dictionary):
    """print"""
-    temp_l = []
-    temp_l.extend(dictionary.keys())
-    #print(temp_l)
-    for i in range(len(temp_l)-1):
-        j = i + 1
-        while j < len(temp_l):
-            char_1 = temp_l[i]
-            char_2 = temp_l[j]
-            if char_1[0] > char_2[0]:
-                temp = temp_l[i]
-                temp_l[i] = temp_l[j]
-                temp_l[j] = temp
+   temp_l = []
+   temp_l.extend(dictionary.keys())
+   #print(temp_l)
+   for i in range(len(temp_l)-1):
+       j = i + 1
+       while j < len(temp_l):
+           char_1 = temp_l[i]
+           char_2 = temp_l[j]
+           if char_1[0] > char_2[0]:
+               temp = temp_l[i]
+               temp_l[i] = temp_l[j]
+               temp_l[j] = temp
             j += 1
     #print(temp_l)
     for each_i in temp_l:

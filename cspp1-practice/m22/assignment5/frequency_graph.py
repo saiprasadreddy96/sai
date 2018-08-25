4b5e86d7e356b55a817118d5c95b5cf0e06a1e18
saiprasadreddy96 <mspreddy1996@msitprogram.net>
2018-08-25 13:28:44 +0530

    " -> 4 of 4 passed. pylint: 7.42/10 "

--- cspp1-practice/m22/assignment5/frequency_graph.py
+++ cspp1-practice/m22/assignment5/frequency_graph.py
@@ -3,26 +3,26 @@ Write a function to print a dictionary with the keys in sorted order along with 
 frequency of each word. Display the frequency values using “#” as a text based graph
 '''
 def frequency_graph(dictionary):
-   """print"""
-   global temp_l
-   temp_l = []
-   temp_l.extend(dictionary.keys())
-   #print(temp_l)
-   for i in range(len(temp_l)-1):
-       j = i + 1
-       while j < len(temp_l):
-           char_1 = temp_l[i]
-           char_2 = temp_l[j]
-           if char_1[0] > char_2[0]:
-               temp = temp_l[i]
-               temp_l[i] = temp_l[j]
-               temp_l[j] = temp
-           elif char_1[0] == char_2[0]:
-               if char_1[1] > char_2[1]:
-                   temp = temp_l[i]
-                   temp_l[i] = temp_l[j]
-                   temp_l[j] = temp
-           j += 1
+    """print"""
+    global temp_l
+    temp_l = []
+    temp_l.extend(dictionary.keys())
+    #print(temp_l)
+    for i in range(len(temp_l)-1):
+        j = i + 1
+        while j < len(temp_l):
+            char_1 = temp_l[i]
+            char_2 = temp_l[j]
+            if char_1[0] > char_2[0]:
+                temp = temp_l[i]
+                temp_l[i] = temp_l[j]
+                temp_l[j] = temp
+            elif char_1[0] == char_2[0]:
+                if char_1[1] > char_2[1]:
+                    temp = temp_l[i]
+                    temp_l[i] = temp_l[j]
+                    temp_l[j] = temp
+            j += 1
 def main():
     """main"""
     global dictionary

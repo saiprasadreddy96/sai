4f76cb781452b1e1867c7bc06518152dfe5984d8
saiprasadreddy96 <mspreddy1996@msitprogram.net>
2018-08-26 23:08:14 +0530

    " -> 2 of 2 passed. pylint: 1.86/10 "

--- cspp1-practice/m20/p1/assignment1.py
+++ cspp1-practice/m20/p1/assignment1.py
@@ -1,23 +1,18 @@
 def merge(arr, l, m, r):
     n1 = (m - l + 1)
     n2 = (r- m)
- 
     # create temp arrays
     L = [0] * (n1)
     R = [0] * (n2)
- 
     # Copy data to temp arrays L[] and R[]
     for i in range(0 , n1):
         L[i] = arr[l + i]
- 
     for j in range(0 , n2):
         R[j] = arr[m + 1 + j]
- 
-    # Merge the temp arrays back into arr[l..r]
-    i = 0     # Initial index of first subarray
-    j = 0     # Initial index of second subarray
-    k = l     # Initial index of merged subarray
- 
+        # Merge the temp arrays back into arr[l..r]
+    i = 0 
+    j = 0 
+    k = l
     while i < n1 and j < n2 :
         if L[i] <= R[j]:
             arr[k] = L[i]
@@ -26,14 +21,12 @@ def merge(arr, l, m, r):
             arr[k] = R[j]
             j += 1
         k += 1
- 
     # Copy the remaining elements of L[], if there
     # are any
     while i < n1:
         arr[k] = L[i]
         i += 1
         k += 1
- 
     # Copy the remaining elements of R[], if there
     # are any
     while j < n2:
@@ -42,7 +35,6 @@ def merge(arr, l, m, r):
         k += 1
 def merge_sort(arr,l,r):
     if l < r:
- 
         # Same as (l+r)/2, but avoids overflow for
         # large l and h
         m = int(l+(r-1))//2
@@ -50,8 +42,6 @@ def merge_sort(arr,l,r):
         merge_sort(arr, l, m)
         merge_sort(arr, m+1, r)
         merge(arr, l, m, r)
- 
- 
 # Driver code to test above
 num = int(input())
 for _ in range(num):

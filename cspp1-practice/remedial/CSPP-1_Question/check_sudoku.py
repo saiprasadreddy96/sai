3ce61710f4a6b6314ccf3be7a8de2f5a6ed38999
saiprasadreddy96 <mspreddy1996@msitprogram.net>
2018-12-21 14:29:25 +0530

    "cspp1_m10_a1_1: 3/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 6/10 "

---
+++ cspp1-practice/remedial/CSPP-1_Question/check_sudoku.py
@@ -0,0 +1,91 @@
+'''
+    Sudoku is a logic-based, combinatorial number-placement puzzle.
+    The objective is to fill a 9×9 grid with digits so that
+    each column, each row, and each of the nine 3×3 subgrids that compose the grid
+    contains all of the digits from 1 to 9.
+
+    Complete the check_sudoku function to check if the given grid
+    satisfies all the sudoku rules given in the statement above.
+'''
+def sudoku1(sudoku):
+    #print(1)
+    for i in range(9):
+        for j in range(9):
+            if (sudoku[i][j] == '.'):
+                sudoku2(sudoku, i, j)
+def sudoku2(sudoku, i, j):
+    a = set()
+    for k in range(9):
+        a.add(sudoku[i][k])
+    for l in range(9):
+        a.add(sudoku[l][j])
+    m = int(i / 3) * 3
+    n = int(j / 3) * 3
+    p = m
+    q = n
+    for p in range(m + 3):
+        for q in range(n + 3):
+            a.add(sudoku[p][q])
+    l = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    for i in range(9):
+        if l[i] not in a:
+            print(l[i])
+
+
+
+
+def check_sudoku(sudoku):
+    '''
+        Your solution goes here. You may add other helper functions as needed.
+        The function has to return True for a valid sudoku grid and false otherwise
+    '''
+    temp_list1 = []
+    temp_list2 = []
+    temp1 = []
+    temp2 = []
+    for each_i in range(len(sudoku)):
+        temp_list1 = sudoku[each_i]
+        temp_list2 = []
+        for each_j in range(9):
+            temp_list2.append(sudoku[each_j][each_i])
+        if  sorted(temp_list1) != [1, 2, 3, 4, 5, 6, 7, 8, 9] or sorted(temp_list2) != [1, 2, 3, 4, 5, 6, 7, 8, 9]:
+            return False
+    for i in range(3):
+        for j in range(3):
+            temp_list3 = []
+            for k in range(3):
+                for l in range(3):
+                    temp_list3.append(sudoku[k+(3*i)][l+(3*j)])
+            if sorted(temp_list3) != [1, 2, 3, 4, 5, 6, 7, 8, 9]:
+                return False
+    return True
+#def sudoku1():
+def main():
+    '''
+        main function to read input sudoku from console
+        call check_sudoku function and print the result to console
+    '''
+    # initialize empty list
+    sudoku = []
+    # loop to read 9 lines of input from console
+    #for i in range(9):
+        # read a line, split it on SPACE and append row to list
+    str = input()
+    #for i in range(9):
+    j = 0
+    k = 9
+    for i in range(9):
+        row = list(str[j:k])
+        sudoku.append(row)
+        j = j + 9
+        k = k + 9
+    #for i in range(9):
+        #print(sudoku[i])
+        
+    # call solution function and print result to console
+    #print(sudoku)
+    #print(check_sudoku(sudoku))
+    sudoku1(sudoku)
+if __name__ == '__main__':
+    main()
+
